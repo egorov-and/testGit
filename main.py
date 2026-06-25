12090966b614bcdfa90acfe2d9a3c11a9230ea58
egorov_and@mail.ru <egorov_and@mail.ru>
2026-06-25 16:46:26 +0500

    25062026-3

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 # Test
-# in new_f changed31
+# in new_f changed311
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('PyCharm2')
